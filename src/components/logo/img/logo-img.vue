a736914a53d20419ea9c7d21d1ca62fb02e5eff7
Samuel Maia <samuelmvf@gmail.com>
2023-09-14 16:52:35 -0300

    feat: criacao do componente de logo baseado em um svg

---
+++ src/components/logo/img/logo-img.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg xmlns:xlink="http://www.w3.org/1999/xlink"
+       xmlns="http://www.w3.org/2000/svg"
+       viewBox="0 0 241.22000122070312 263.6000061035156"
+       fill="#006BCB" width="43.92473365135983"  height="48" >
+    <g fill="#006BCB">
+      <g fill="#006BCB">
+        <path fill="#00AEFF" d="M119.36 218.75l30.68 44.85H88.69l-30.67-44.85h61.34z"></path>
+        <path fill="#006BCB" d="M61.35 133.76L152.82 0H91.47L0 133.76h61.35z"></path>
+        <path fill="#00AEFF" d="M105.72 198.64H44.37L0 133.76h61.35l44.37 64.88z"></path>
+        <path fill="#006BCB" d="M179.88 130.26L88.69 263.6h61.35l91.18-133.34h-61.34zM105.72 198.64l91.13-133.27-61.34.01-91.14 133.26h61.35z"></path>
+        <path fill="#00AEFF" d="M241.22 130.26h-61.34L135.5 65.37h61.35l44.37 64.89zM152.82 0l30.67 44.85h-61.35L91.47 0h61.35z"></path>
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'logo-img'
+}
+</script>

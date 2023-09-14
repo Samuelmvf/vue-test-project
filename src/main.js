24ebaf28ec98fcd08d243bcf6e5a9df7718e88e5
Samuel Maia <samuelmvf@gmail.com>
2023-09-14 11:51:12 -0300

    chore: adição e configuração do vuetify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import { appRouter } from "@/routes"
+import { appVuetify } from "@/boot/vuetify"
+
 import './styles/global.scss'
 
 import App from './App.vue'
 
-createApp(App).use(appRouter).mount('#app')
+createApp(App)
+    .use(appVuetify)
+    .use(appRouter)
+    .mount('#app')

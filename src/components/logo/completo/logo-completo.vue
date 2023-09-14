b147cbc051078414ada369a9137ca4ac05f6d54b
Samuel Maia <samuelmvf@gmail.com>
2023-09-14 16:54:07 -0300

    feat: criacao do componente para uma variante da logo

---
+++ src/components/logo/completo/logo-completo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="d-flex align-items-center">
+    <logo-img />
+    <h1 class="ml-2 text-title user-select-none">SAMSystem</h1>
+  </div>
+</template>
+
+<script>
+import LogoImg from "@/components/logo/img/logo-img.vue";
+
+export default {
+  name: 'logo-completo',
+  components: { LogoImg }
+}
+</script>

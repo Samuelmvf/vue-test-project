2c66dccdc0b27013180f18106c21ab9c2eb984d7
Samuel Maia <samuelmvf@gmail.com>
2023-09-14 10:59:37 -0300

    chore: adição e configuração do vue-router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { appRouter } from "@/routes"
 import './styles/global.scss'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+
+createApp(App).use(appRouter).mount('#app')
---
+++ src/routes/routes.js
@@ -0,0 +1,11 @@
+import PageCliente from "@/pages/page-cliente.vue";
+
+const routes = [
+  {
+    path: '/',
+    component: PageCliente,
+    children: []
+  }
+]
+
+export default routes

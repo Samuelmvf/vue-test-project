495835c26cebac1f5ca7ab67834958ec2f5006dc
Samuel Maia <samuelmvf@gmail.com>
2023-09-15 17:54:08 -0300

    feat: criação do componente footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <app-header />
   <router-view></router-view>
+  <app-footer />
 </template>
 
 <script>
 import AppHeader from "@/components/app-header/app-header.vue";
+import AppFooter from "@/components/app-footer/app-footer.vue";
 
 export default {
   name: 'App',
-  components: { AppHeader },
+  components: { AppHeader , AppFooter },
 }
 </script>

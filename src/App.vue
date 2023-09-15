d16573434d8e0afda8dc2d3ec9951fb9066e2124
Samuel Maia <samuelmvf@gmail.com>
2023-09-15 10:38:07 -0300

    feat: criação do escopo inicial do componente de header da aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
+  <app-header />
   <router-view></router-view>
 </template>
 
 <script>
+import AppHeader from "@/components/app-header/app-header.vue";
+
 export default {
   name: 'App',
+  components: { AppHeader },
 }
 </script>
---
+++ src/components/app-header/app-header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="bg-primary">
+    <nav class="px-4 max-app-size d-flex justify-content-between">
+      <logo-completo />
+      <menu-web />
+    </nav>
+  </header>
+</template>
+
+<script>
+import LogoCompleto from "@/components/logo/completo/logo-completo.vue";
+import MenuWeb from "@/components/menu/web/menu-web.vue";
+
+export default {
+  name: 'app-header',
+  components: { LogoCompleto, MenuWeb },
+  data () {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  height: 4.625rem;
+  padding: .625rem 0;
+
+  nav {
+    height: 100%;
+    margin: 0 auto;
+  }
+}
+</style>

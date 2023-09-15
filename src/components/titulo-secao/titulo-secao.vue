07aa5cd84250c8972a80d83f7d583c263cd0803d
Samuel Maia <samuelmvf@gmail.com>
2023-09-15 20:07:49 -0300

    feat: criação do componente de titulo

---
+++ src/components/titulo-secao/titulo-secao.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="d-flex position-relative box-titulo">
+    <span class="bg-primary-subtle pa-3 position-absolute">///</span>
+    <h1>{{ texto }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'titulo-secao',
+  props: {
+    texto: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.box-titulo {
+  span {
+    z-index: 1;
+    border-radius: 0.35rem;
+    user-select: none;
+  }
+  h1 {
+    font-family: $fontTitle;
+    font-size: 2.5rem;
+    text-transform: uppercase;
+    z-index: 2;
+    margin: .8rem 0 0 1rem;
+  }
+}
+</style>

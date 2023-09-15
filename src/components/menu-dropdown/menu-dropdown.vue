7f239c7fd00a9e6ad6c94cb093ab3608b22a6372
Samuel Maia <samuelmvf@gmail.com>
2023-09-15 10:37:05 -0300

    feat: criação do componente menu-dropdown

---
+++ src/components/menu-dropdown/menu-dropdown.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-menu
+    open-on-hover
+  >
+    <template v-slot:activator="{ props }">
+      <v-btn
+        v-bind="props"
+        variant="text"
+        :prepend-icon="icon ? icon : ''"
+      >
+        {{ nome }}
+      </v-btn>
+    </template>
+
+    <v-list density="compact">
+      <v-list-subheader v-if="subtitulo">{{ subtitulo }}</v-list-subheader>
+
+      <v-list-item
+        v-for="(item, i) in actions"
+        :key="i"
+        :value="item"
+        color="primary"
+      >
+        <template v-slot:prepend>
+          <v-icon :icon="item.icon"></v-icon>
+        </template>
+
+        <v-list-item-title v-text="item.nome"></v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: 'menu-dropdown',
+  props: {
+    nome: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: false
+    },
+    actions: {
+      type: Array,
+      required: true
+    },
+    subtitulo: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>

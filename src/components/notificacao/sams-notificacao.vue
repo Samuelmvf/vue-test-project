7f452d533b47bdb1e9344db0625b74b89044eb23
Samuel Maia <samuelmvf@gmail.com>
2023-09-18 15:49:39 -0300

    feat: criação do componente de notificação

---
+++ src/components/notificacao/sams-notificacao.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- TODO implementar um componente de notificação que permite multiplas notificações -->
+  <v-snackbar
+    max-height="60"
+    v-model="computedNotification.ativa"
+    v-bind="computedNotification.propriedades"
+    @update:modelValue="limparNotificacao"
+  >
+    {{ computedNotification.mensagem }}
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: 'sams-notificacao',
+  props: {
+    notificacao: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    computedNotification () {
+      return this.notificacao
+    }
+  },
+  methods: {
+    limparNotificacao (val) {
+      console.log(val)
+      if (!this.computedNotification.ativa)
+        this.$emit('resetNotification')
+    }
+  }
+}
+</script>

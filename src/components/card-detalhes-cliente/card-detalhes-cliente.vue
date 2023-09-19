d153237f1f02f07e4cbaa23e4da8a3c015db1b2f
Samuel Maia <samuelmvf@gmail.com>
2023-09-19 15:50:47 -0300

    feat: criação do componente card-detalhes-cliente e finalização pagina associação de produto

---
+++ src/components/card-detalhes-cliente/card-detalhes-cliente.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="col-12" >
+    <h1>INFORMAÇÕES DO CLIENTE</h1>
+    <v-card class="pa-4 mx-3 mt-2">
+      <p><span class="font-weight-medium text-client-attributes">Nome: </span>{{ nome }}</p>
+      <p><span class="font-weight-medium text-client-attributes">Documento: </span>{{ documento }}</p>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'card-detalhes-cliente',
+  props: {
+    nome: {
+      type: String,
+      required: true
+    },
+    documento: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+h1 {
+  font-family: $fontTitle;
+  font-size: 2rem;
+  text-transform: uppercase;
+}
+
+.text-client-attributes {
+  font-family: $fontTitle;
+  text-transform: uppercase;
+}
+</style>
--- src/pages/cliente/associar-produto/page-associar-produto.vue
+++ src/pages/cliente/associar-produto/page-associar-produto.vue
@@ -1,14 +1,99 @@
 <template>
   <div class="center max-app-size py-10 px-4">
     <titulo-secao texto="Associar produto"/>
+    <v-card
+      class="mt-7"
+    >
+      <v-card-item class="row">
+        <card-detalhes-cliente :nome="cliente.nome" :documento="cliente.documento" />
+        <v-divider class="my-3"></v-divider>
+        <associador-entidades
+          v-if="todosProdutos.length"
+          :entidadadesAssociadas="cliente.produtos"
+          :entidadesDisponiveis="produtosDisponiveis"
+          class="col row ma-4"
+          @atualizarAssociacoes="atualizarAssociacoes"
+        />
+      </v-card-item>
+
+      <v-card-actions class="form-card-actions">
+        <v-btn @click="salvarAssociacoes" variant="elevated" color="success">Salvar produtos associados</v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
 <script>
-import TituloSecao from "@/components/titulo-secao/titulo-secao.vue";
+
+import { buscarClientePorId, editarCliente } from "@/services/cliente-service"
+import { buscarTodosProdutos } from "@/services/produto-service"
+
+import { NOTIFICACAO_TIPOS } from "@/contants/constantes-notificacao"
+
+import TituloSecao from "@/components/titulo-secao/titulo-secao.vue"
+import CardDetalhesCliente from "@/components/card-detalhes-cliente/card-detalhes-cliente.vue"
+import AssociadorEntidades from "@/components/associador-entidades/associador-entidades.vue"
 
 export default {
   name: 'page-associar-produto',
-  components: { TituloSecao }
+  components: { AssociadorEntidades, CardDetalhesCliente, TituloSecao },
+  inject: ['setAppLoading', 'emitirNotificacao'],
+
+  data: () => ({
+    cliente: {
+      nome: '',
+      documento: '',
+      produtos: []
+    },
+    todosProdutos: []
+  }),
+
+  computed: {
+    produtosDisponiveis () {
+      return this.todosProdutos.filter(({ id }) => !this.cliente.produtos.some(cliente => cliente.id === id))
+    }
+  },
+
+  methods: {
+    buscarRecursosAssociacaoProdutos () {
+      this.setAppLoading(true)
+      Promise.all([buscarClientePorId(this.$route.params.id), buscarTodosProdutos()])
+        .then(responses => {
+          this.setAppLoading(false)
+          this.cliente = responses[0].data.cliente
+          this.todosProdutos = responses[1].data.produtos
+        })
+        .catch(() => {
+          this.setAppLoading(false)
+          this.emitirNotificacao(NOTIFICACAO_TIPOS.ERRO, 'Falha ao carregar informações de associção de produto, tente novamente mais tarde.')
+          this.$router.push('/cliente')
+        })
+
+    },
+
+    atualizarAssociacoes (novosProdutos) {
+      this.cliente.produtos = []
+      this.cliente.produtos = novosProdutos
+    },
+
+    salvarAssociacoes () {
+      this.setAppLoading(true)
+      editarCliente(this.cliente)
+        .then(() => {
+          this.setAppLoading(false)
+          this.emitirNotificacao(NOTIFICACAO_TIPOS.SUCESSO, 'Associações com produtos atualizadas com sucesso!')
+          this.$router.push('/cliente')
+        })
+        .catch(() => {
+          this.setAppLoading(false)
+          this.emitirNotificacao(NOTIFICACAO_TIPOS.ERRO, 'Falha ao atualizar associações, tente novamente mais tarde!')
+        })
+    }
+  },
+
+  mounted () {
+    this.buscarRecursosAssociacaoProdutos()
+  }
 }
 </script>
+

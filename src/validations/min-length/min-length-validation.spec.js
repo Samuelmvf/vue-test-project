9ffe271f87878d6254fcf1478a5da730ae1dbf37
Samuel Maia <samuelmvf@gmail.com>
2023-09-28 15:53:00 -0300

    test: criação da classe de validação de min-length

---
+++ src/validations/min-length/min-length-validation.spec.js
@@ -0,0 +1,20 @@
+import { MinLengthValidation } from "@/validations"
+
+const makeSut = () => {
+  const minLength = 10
+  return new MinLengthValidation('texto', minLength)
+}
+
+describe('MinLengthValidation', () => {
+  it('Deve retornar um Error caso o texto não possua o tamanho mínimo de caracteres', () => {
+    const sut = makeSut()
+    const retornoValidacao = sut.validate('error')
+    expect(retornoValidacao).toBe(sut.message)
+  });
+
+  it('Deve retornar true caso o texto possua o tamanho minimo de caracteres.', () => {
+    const sut = makeSut()
+    const retornoValidacao = sut.validate('test_value')
+    expect(retornoValidacao).toBe(true)
+  });
+})

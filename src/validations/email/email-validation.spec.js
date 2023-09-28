eb1a623f516b6ab4d8a0866a04fbdae0b9629041
Samuel Maia <samuelmvf@gmail.com>
2023-09-28 15:51:41 -0300

    test: criação da classe de validação de email

---
+++ src/validations/email/email-validation.spec.js
@@ -0,0 +1,16 @@
+import { EmailValidation } from "@/validations"
+import { faker } from "@faker-js/faker";
+
+describe('EmailValidation', () => {
+  it('Deve retornar um Error caso o email esteja inválido', () => {
+    const sut = new EmailValidation('email')
+    const error = sut.validate('error')
+    expect(error).toBe(sut.message)
+  })
+
+  it('Deve retornar true caso o email esteja válido', () => {
+    const sut = new EmailValidation('email')
+    const campoValido = sut.validate(faker.internet.email())
+    expect(campoValido).toBe(true)
+  })
+})

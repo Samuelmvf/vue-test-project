ec44fc842dae37c2c3e8d04f96b30fa7823b5f26
Samuel Maia <samuelmvf@gmail.com>
2023-09-28 15:52:16 -0300

    test: criação da classe de validação de campos obrigatórios

---
+++ src/validations/index.js
@@ -0,0 +1,4 @@
+export * from './email/email-validation'
+export * from './max-length/max-length-validation'
+export * from './min-length/min-length-validation'
+export * from './required-field/required-field-validation'
---
+++ src/validations/required-field/required-field-validation.spec.js
@@ -0,0 +1,17 @@
+import { RequiredFieldValidation } from "@/validations";
+
+const makeSut = () => new RequiredFieldValidation('campo')
+
+describe('RequiredFieldValidation', () => {
+  it('Deve retornar um Error caso o campo esteja vazio', () => {
+    const sut = makeSut()
+    const retornoValidacao = sut.validate('')
+    expect(retornoValidacao).toBe(sut.message)
+  });
+
+  it('Deve retornar true caso o campo esteja valido', () => {
+    const sut = makeSut()
+    const retornoValidacao = sut.validate('valor_valido')
+    expect(retornoValidacao).toBe(true)
+  });
+})
